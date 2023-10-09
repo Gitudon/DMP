74f5c85e2994626652b3661d813286bf40846808
Gitudon <oniku.udon.029@gmail.com>
2023-10-09 10:06:20 +0900

    commit

--- GUI/game/cards.py
+++ GUI/game/cards.py
@@ -24,5 +24,17 @@ def b_s_001(save,flag):
 
 def b_c_003_cip(save,flag):
     if flag:
-        temp=save[0]
-    return
+        temp=save[0]+save[10]
+        save[0]=func.shuffle(temp)
+        save[10]=[]
+    else:
+        temp=save[1]+save[11]
+        save[1]=func.shuffle(temp)
+        save[11]=[]
+    return save
+
+def b_c_001_cip(save,flag):
+    if flag:
+        if len(save[0])>5:
+            temp=save[:5]
+    return save
