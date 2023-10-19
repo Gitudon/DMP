ce54f8e5669d78584f43737687e45fdcbc0a65c9
Gitudon <oniku.udon.029@gmail.com>
2023-10-19 13:50:04 +0900

    commit

--- GUI/etc/deckbuild.py
+++ GUI/etc/deckbuild.py
@@ -1,15 +1,13 @@
 import json
 import codecs
-file=open('DMP/GUI/etc/build.json','r',encoding="utf_8")
+file=open('DMP/GUI/etc/cardlist.json','r',encoding="utf_8")
 loading=json.load(file)
 deck=[]
-card=[0]*12  #要素が増えたら更新
-
+card=[0]*13  #要素が増えたら更新
 li=[]
 #ここに登録するデッキリストのパスを入れる
 with open('DMP/GUI/decks/decklist.txt','r',encoding='utf-8') as f:
     li=f.read().split()
-
 for names in li:
     i=0
     temp=loading[names]
