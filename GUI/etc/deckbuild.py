62d27352b219c0bf38959ebcfdde0dfd9493c8cd
Gitudon <oniku.udon.029@gmail.com>
2023-10-19 13:31:13 +0900

    デッキリスト作成部分の実装

--- GUI/etc/deckbuild.py
+++ GUI/etc/deckbuild.py
@@ -1,9 +1,20 @@
 import json
-file=open("build.json","r")
+import codecs
+file=open('DMP/GUI/etc/build.json','r',encoding="utf_8")
 loading=json.load(file)
-card=[0]*12 #要素が増えたら更新
-i=0
-for value in loading:
-    card[i]=value
-    i+=1
-print(card)
+deck=[]
+card=[0]*12  #要素が増えたら更新
+
+li=[]
+#ここに登録するデッキリストのパスを入れる
+with open('DMP/GUI/decks/decklist.txt','r',encoding='utf-8') as f:
+    li=f.read().split()
+
+for names in li:
+    i=0
+    temp=loading[names]
+    for vals in temp:
+        card[i]=temp[vals]
+        i+=1
+    deck.append(card)
+print(deck,file=codecs.open('DMP/GUI/etc/output.txt','w','utf-8'))
